733881238892cbbc1c0ec20c4664e0cb30c5d305
nghiaho310pf <164844348+nghiaho310pf@users.noreply.github.com>
2026-03-27 10:26:09 +0700

    Split depth_shader to files

--- src/shading/shaders/depth_shader.py
+++ src/shading/shaders/depth_shader.py
@@ -1,35 +1,12 @@
+from pathlib import Path
+
 from shading.shader import Shader
-
-VERTEX_SHADER = """
-#version 450 core
-layout (location = 0) in vec3 a_Pos;
-
-uniform mat4 u_Projection;
-uniform mat4 u_View;
-uniform mat4 u_Model;
-
-out vec3 v_FragPos;
-
-void main() {
-    v_FragPos = vec3(u_Model * vec4(a_Pos, 1.0));
-    gl_Position = u_Projection * u_View * vec4(v_FragPos, 1.0);
-}
-"""
-
-FRAGMENT_SHADER = """
-#version 450 core
-in vec3 v_FragPos;
-
-uniform vec3 u_LightPos;
-uniform float u_FarPlane;
-
-void main() {
-    float lightDistance = length(v_FragPos - u_LightPos);
-    lightDistance = lightDistance / u_FarPlane;
-    gl_FragDepth = lightDistance;
-}
-"""
+from shading.src_utils import read_source_file
 
 
 def make_shader():
-    return Shader(VERTEX_SHADER, FRAGMENT_SHADER)
+    p = Path(__file__).parent.absolute()
+    return Shader(
+        read_source_file(p / "depth_shader/vert.glsl"),
+        read_source_file(p / "depth_shader/frag.glsl")
+    )
