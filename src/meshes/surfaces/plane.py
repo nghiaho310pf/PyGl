cf154be00e9736faa15a039d56626ad724d8aa69
nghiaho310pf <164844348+nghiaho310pf@users.noreply.github.com>
2026-03-29 18:44:01 +0700

    Add the other face of the plane

--- src/meshes/surfaces/plane.py
+++ src/meshes/surfaces/plane.py
@@ -1,13 +1,10 @@
-import math
-
 import numpy as np
-
 
 def generate_plane(size=10.0, uv_scale=1.0):
     """
     Generates a flat plane (floor) on the XZ plane.
-    Total vertices: 4
-    Total indices: 6 (2 triangles)
+    Total vertices: 8 (4 for top, 4 for bottom)
+    Total indices: 12 (4 triangles)
 
     Returns:
         vertices: np.array (float32) -> Interleaved [Pos(3), Norm(3), UV(2)]
@@ -20,11 +17,17 @@ def generate_plane(size=10.0, uv_scale=1.0):
         -s, 0.0, s, 0, 1, 0, 0, 0,
         s, 0.0, s, 0, 1, 0, uv_scale, 0,
         s, 0.0, -s, 0, 1, 0, uv_scale, uv_scale
+        -s, 0.0, -s, 0, -1, 0, 0, uv_scale,
+        -s, 0.0, s, 0, -1, 0, 0, 0,
+        s, 0.0, s, 0, -1, 0, uv_scale, 0,
+        s, 0.0, -s, 0, -1, 0, uv_scale, uv_scale
     ]
 
     indices = [
         0, 1, 2,
-        0, 2, 3
+        0, 2, 3,
+        4, 6, 5,
+        4, 7, 6
     ]
 
     return np.array(data, dtype=np.float32), np.array(indices, dtype=np.uint32)
