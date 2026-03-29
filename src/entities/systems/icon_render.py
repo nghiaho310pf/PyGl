9635492fe7824c3fec8ea6892d9da0ede3114c4f
nghiaho310pf <164844348+nghiaho310pf@users.noreply.github.com>
2026-03-29 12:58:49 +0700

    Bells and whistles for icon renderer

--- src/entities/systems/icon_render.py
+++ src/entities/systems/icon_render.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from imgui_bundle import imgui, icons_fontawesome_6
 
@@ -6,7 +5,6 @@ from entities.components.camera_state import CameraState
 from entities.components.icon_render_state import IconRenderState
 from entities.components.point_light import PointLight
 from entities.components.ui_state import UiState
-import math_utils
 from entities.components.camera import Camera
 from entities.components.transform import Transform
 from entities.components.render_state import RenderState
@@ -16,9 +14,8 @@ from entities.registry import Registry
 class IconRenderSystem:
     @staticmethod
     def update(registry: Registry, window_size: tuple[int, int]):
-        window_width, window_height = window_size
-
-        r_admin = registry.get_singleton(IconRenderState, RenderState, CameraState, UiState)
+        r_admin = registry.get_singleton(
+            IconRenderState, RenderState, CameraState, UiState)
         if r_admin is None:
             return
         _, (icon_render_state, render_state, camera_state, ui_state) = r_admin
@@ -29,54 +26,61 @@ class IconRenderSystem:
             for entity, (transform, camera) in registry.view(Transform, Camera):
                 if entity == camera_state.target_camera:
                     continue
-
-                world_pos = np.array(
-                    [transform.position[0], transform.position[1], transform.position[2], 1.0])
-                clip_pos = world_pos @ camera_state.view_projection_matrix
-
-                if clip_pos[3] <= 0:
-                    continue
-
-                ndc = clip_pos[:3] / clip_pos[3]
-
-                if abs(ndc[0]) > 1.0 or abs(ndc[1]) > 1.0:
-                    continue
-
-                screen_x = (ndc[0] + 1.0) * 0.5 * window_width
-                screen_y = (1.0 - ndc[1]) * 0.5 * window_height
-
-                icon = icons_fontawesome_6.ICON_FA_CAMERA
-                text_size = imgui.calc_text_size(icon)
-
-                pos = (screen_x - text_size.x * 0.5, screen_y - text_size.y * 0.5)
-                selected = entity == ui_state.selected_entity
-                color = imgui.Col_.text if selected else imgui.Col_.text_disabled
-                draw_list.add_text(pos, imgui.get_color_u32(color), icon)
+                IconRenderSystem._draw_entity_icon(
+                    draw_list, transform, camera_state.view_projection_matrix, window_size, 
+                    entity == ui_state.selected_entity, icons_fontawesome_6.ICON_FA_CAMERA,
+                    np.array([1.0, 1.0, 1.0])
+                )
 
         if icon_render_state.draw_light_icons:
             for entity, (transform, light) in registry.view(Transform, PointLight):
                 if entity == camera_state.target_camera:
                     continue
+                IconRenderSystem._draw_entity_icon(
+                    draw_list, transform, camera_state.view_projection_matrix, window_size, 
+                    entity == ui_state.selected_entity, icons_fontawesome_6.ICON_FA_LIGHTBULB,
+                    light.color * 0.6 + np.array([1.0, 1.0, 1.0]) * 0.4
+                )
 
-                world_pos = np.array(
-                    [transform.position[0], transform.position[1], transform.position[2], 1.0])
-                clip_pos = world_pos @ camera_state.view_projection_matrix
+    @staticmethod
+    def _draw_entity_icon(
+        draw_list: imgui.ImDrawList,
+        transform: Transform, view_projection_matrix: np.ndarray,
+        window_size: tuple[int, int],
+        selected: bool, icon: str,
+        color: np.ndarray
+    ):
+        world_pos = np.array(
+            [transform.position[0], transform.position[1], transform.position[2], 1.0])
+        clip_pos = world_pos @ view_projection_matrix
 
-                if clip_pos[3] <= 0:
-                    continue
+        if clip_pos[3] <= 0:
+            return
 
-                ndc = clip_pos[:3] / clip_pos[3]
+        ndc = clip_pos[:3] / clip_pos[3]
 
-                if abs(ndc[0]) > 1.0 or abs(ndc[1]) > 1.0:
-                    continue
+        if abs(ndc[0]) > 1.0 or abs(ndc[1]) > 1.0:
+            return
 
-                screen_x = (ndc[0] + 1.0) * 0.5 * window_width
-                screen_y = (1.0 - ndc[1]) * 0.5 * window_height
+        window_width, window_height = window_size
+        screen_x = (ndc[0] + 1.0) * 0.5 * window_width
+        screen_y = (1.0 - ndc[1]) * 0.5 * window_height
 
-                icon = icons_fontawesome_6.ICON_FA_LIGHTBULB
-                text_size = imgui.calc_text_size(icon)
+        scale = 1.0 if selected else 0.8
+        base_size = imgui.calc_text_size(icon)
 
-                pos = (screen_x - text_size.x * 0.5, screen_y - text_size.y * 0.5)
-                selected = entity == ui_state.selected_entity
-                color = imgui.Col_.text if selected else imgui.Col_.text_disabled
-                draw_list.add_text(pos, imgui.get_color_u32(color), icon)
+        alpha = 1.0 if selected else 0.7
+        icon_color_u32 = imgui.get_color_u32(
+            (float(color[0]), float(color[1]), float(color[2]), alpha)
+        )
+
+        draw_list.add_text(
+            imgui.get_font(),
+            imgui.get_font_size() * scale,
+            (
+                screen_x - (base_size.x * scale) * 0.5,
+                screen_y - (base_size.y * scale) * 0.5
+            ),
+            icon_color_u32,
+            icon
+        )
